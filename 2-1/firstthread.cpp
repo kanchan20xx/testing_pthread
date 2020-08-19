77f8c7f63982e2edeb176f1ae3648a29c91a4f3d
kk <kk@kknoMacBook-Pro.local>
2020-08-19 22:29:36 +0900

    Added a source file for 2-1.

---
+++ 2-1/firstthread.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+#include <pthread.h>
+#include <chrono>
+
+void *threadFunc(void *arg)
+{
+    int i;
+    for(i = 0; i < 3; i++) {
+        std::cout << "I'm threadFunc" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    return nullptr;
+}
+
+int main()
+{
+    pthread_t thread;
+    int i;
+
+    if(pthread_create(&thread, nullptr, threadFunc, nullptr) != 0) {
+        exit(1);
+    }
+
+    for(i = 0; i < 5; i++) {
+        std::cout << "I'm main." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    return 0;
+}
